33acf90788693abba07fe30093f1710dc9452e01
Darren Siu <mcsiu@berkeley.edu>
2024-08-25 01:08:08 -0700

    test2

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, "dist")));
 
-// All other routes should return the index.html file to support client-side routing
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// Use environment variable PORT or default to 3000
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
