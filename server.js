277de5fb1746eec41e90e4e02232b834f98adb68
Darren Siu <mcsiu@berkeley.edu>
2024-08-25 01:00:24 -0700

    start with vite

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import {fileURLToPath} from "url";
+
 const app = express();
+
+// Determine the __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, "dist")));
@@ -11,7 +17,7 @@ app.get("*", (req, res) => {
 });
 
 // Set the port dynamically with an environment variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
