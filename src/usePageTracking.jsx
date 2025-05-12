660bce78ee16cc06f70928ab6b82bc15aad780f6
Darren <mcsiu@berkeley.edu>
2025-05-12 00:54:43 -0700

    test

--- src/usePageTracking.jsx
+++ src/usePageTracking.jsx
@@ -1,15 +1,19 @@
 import {useLocation} from "react-router-dom";
 import {useEffect} from "react";
 
+const GA_MEASUREMENT_ID = "G-N729BR6ZKZ";
+
 export default function usePageTracking() {
-    const location = useLocation();
+    const {pathname} = useLocation();
 
     useEffect(() => {
         if (window.gtag) {
-            window.gtag("event", "page_view", {
-                page_path: location.pathname,
+            console.log("📈 sending page_view for:", pathname);
+
+            window.gtag("config", GA_MEASUREMENT_ID, {
+                page_path: pathname,
                 page_title: document.title,
             });
         }
-    }, [location]);
+    }, [pathname]);
 }
