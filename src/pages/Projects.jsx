4478865927fabcf6a044b80b6e1bf3b6b1ecd4ee
Darren <mcsiu@berkeley.edu>
2025-07-27 22:05:03 -0700

    fixed typo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,9 +13,9 @@ function Projects() {
                         with a twist: once there are more than six tiles on the board, the oldest tile disappears.
                         Curious whether this version is also a solved game like the classic one—which results in a
                         forced draw with perfect play—I built an online version and implemented a Minimax AI to explore
-                        it further. Through analysis, I discovered that this variant has a forced win in 13 moves from
+                        it further. Through analysis, I discovered that this variant has a forced win in 7 moves from
                         the initial position. However, if the first move is restricted to the center or a corner—rather
-                        than the side edge, which is required for the 13-move forced win—the game results in a forced
+                        than the side edge, which is required for the 7-move forced win—the game results in a forced
                         draw due to repetition. A detailed explanation, including the mathematical reasoning, is
                         available in the Academic & Technical Writing section of the website.
                     </p>
